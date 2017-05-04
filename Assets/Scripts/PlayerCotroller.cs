e1038020146702edcbfcf2e887e8eaeac824a1a7
Plug-04 <c.putiphon@gmail.com>
2017-05-04 15:45:27 +0700

    overlay added

---
+++ Assets/Scripts/PlayerCotroller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCotroller : MonoBehaviour {
+
+    public Text lifeText;
+    public Text wavesText;
+    public Text expText;
+
+    private int life;
+    private int waves;
+    private int exp;
+
+    // Use this for initialization
+    void Start () {
+        life = 20;
+        waves = 0;
+        exp = 0;
+        SetText();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SetText()
+
+    {
+        lifeText.text = "Life: " + life.ToString();
+        wavesText.text = "Waves: " + waves.ToString();
+        expText.text = "Exp: " + exp.ToString();
+    }
+
+
+}
